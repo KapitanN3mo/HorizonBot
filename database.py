9957b4393972026ce83ac5b81277b7a547d0e76b
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-10-28 00:21:03 +0300

    Изменена труктура БД

--- database.py
+++ database.py
@@ -2,6 +2,15 @@ import sqlite3
 
 db = sqlite3.connect('database.db')
 cursor = db.cursor()
-cursor.execute('''CREATE IF NOT EXISTS users (
-id PRIMARY KEY,
-)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS server_users(
+id INTEGER PRIMARY KEY,
+message_count INTEGER NOT NULL,
+xp INTEGER NOT NULL,
+in_voice_time INT NOT NULL,
+status TEXT,
+warns TEXT,
+sys_info TEXT,
+last_voice_time
+);''')
+db.commit()
+
