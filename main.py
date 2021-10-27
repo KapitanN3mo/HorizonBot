e9baf4a8881fdf9d645f357eff3b01654fa39e46
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-10-28 00:22:03 +0300

    Правки для логов (которые не работают) и чтение токена из конфига

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from discord.ext import commands
-import config
 import discord
 from discord_components import DiscordComponents
 import os
+from componets import config
+import logging
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='h.', intents=intents)
 bot.remove_command('help')
 DiscordComponents(bot=bot)
 
-
-@bot.event
-async def on_ready():
-    print('ready')
-
-
+logger = logging.getLogger('MODULE_LOADER')
 for module in os.listdir('modules'):
     if module.endswith('.py'):
-        print(f'Загружен модуль {module}')
+        logger.info(f'Загружен модуль {module}')
         bot.load_extension(f'modules.{module.replace(".py", "")}')
-bot.run(config.token)
+token = config.get('Global', 'token')
+bot.run(token)
