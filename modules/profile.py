c9a70348ce00cc236f2149c2d02401587631aeb1
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-10-30 02:03:44 +0300

    Исправлена ошибка с опытом

--- modules/profile.py
+++ modules/profile.py
@@ -10,22 +10,20 @@ class ProfileModule(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def profile(self, ctx, user=None):
-        print('profile')
+    async def profile(self, ctx: commands.Context, user: discord.User or None = None):
         if user is None:
             user = ctx.author
         cursor.execute(
-            f'SELECT message_count,xp,in_voice_time,status,warns,last_voice_time FROM server_users WHERE id = {ctx.author.id}')
+            f'SELECT message_count,xp,in_voice_time,status,warns,last_voice_time FROM server_users WHERE id = {user.id}')
         result = cursor.fetchone()
         if result is None:
             await ctx.send('Это ваше первое сообщение! Ваш профиль создан!')
 
         else:
             message_count, xp, in_voice_time, status, warns, last_voice_time = result
-            embed = discord.Embed(title=f'Профиль пользователя {ctx.author.name}', colour=discord.Colour.random())
+            embed = discord.Embed(title=f'Профиль пользователя {user.name}', colour=discord.Colour.random())
             embed.add_field(name='Количество сообщений', value=message_count)
-            xp_multiplier = float(config.get('Profile', 'xp_message_multiplier')) # ЧТЕНИЕ ИЗ БД
-            embed.add_field(name='Очки опыта', value=message_count * xp_multiplier)
+            embed.add_field(name='Очки опыта', value=xp)
             embed.add_field(name='Время в голосовом канале', value=f'{math.ceil(in_voice_time / 60)} минут')
             await ctx.send(embed=embed)
 
