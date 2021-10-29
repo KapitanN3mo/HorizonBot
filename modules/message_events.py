212fe76108cdb39ac52f1c1222897e65511b6f29
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-10-30 02:03:20 +0300

    Исправлена ошибка с опытом

--- modules/message_events.py
+++ modules/message_events.py
@@ -1,6 +1,9 @@
+import math
 import discord
 from discord.ext import commands
 from database import cursor, db
+from componets import config
+import re
 
 
 class EventsModule(commands.Cog):
@@ -9,19 +12,31 @@ class EventsModule(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        cursor.execute(f'SELECT message_count FROM server_users WHERE id = {message.author.id}')
+        cursor.execute(f'SELECT message_count,xp FROM server_users WHERE id = {message.author.id}')
         result = cursor.fetchone()
-        print(result)
         if result is None:
-            user_data = [message.author.id, 1, 0, 0, 'empty', 'empty', 'empty', 'empty']
+            user_data = [message.author.id, 1, 1, 0, 'normal', '{}', '{"send_dm_voice":"false"}', 'none']
             cursor.execute(f'INSERT INTO server_users VALUES(?,?,?,?,?,?,?,?)', user_data)
             db.commit()
         else:
-            result = result[0]
-            result += 1
-            cursor.execute(f'UPDATE server_users SET message_count = {result} WHERE id = {message.author.id}')
+            old_count = result[0]
+            old_xp = result[1]
+            old_count += 1
+            xp_multiplier = int(config.get('Profile', 'xp_message_multiplier'))
+            total_xp = int(xp_multiplier + old_xp)
+            cursor.execute(
+                f'''UPDATE server_users SET message_count = {old_count},xp = {total_xp} WHERE id = {message.author.id}''')
             db.commit()
-        # await self.bot.process_commands(message)
+
+
+def mat_search(text):
+    return re.findall(
+        r'[хxh][уyu][юйиuiяеeё]|[еe][б6b][аa@uio0иоуyкkнhnlлсcтыbь]|ё[б6b][аa@uio0иоуyкkнhnlлсcтыbь]|д.л[б6b]'
+        r'[аa@o0оыbьееёиui]|[дdаa@ыьъo0о][еeё][б6b]|[еe][б6b][ееё][тцмш]|[еe][б6b][ееё]н[ьbяееиuiн]|[сc]ц?[уyыb][кч4]|'
+        r'[б6b]л[яэ]|г.ндон|г.вн|моч[аa@]|жоп|п[ееёиui]зд|cum|конч|[сc][сcц]ц?[уyаa@ыb]|м[аa@]нд|м[иui]н[еe]т|[б6b]зд.|др'
+        r'[иuiее][сc][тн]|[сc][иui]р[ыьbаa@][ткл]|[сc]р[аa@][ткл]|[хxh][еe]р|п[иui]д.р|fu[сc]k|[сc]unt|[сc]ock|др[аa@o0о]'
+        r'[тч]|п[еe]рд|св[аa@o0о]л[аa@o0о]|[еe]лд[аa@]|муд[аa@иuio0оeе]|з[аa@o0о]луп|п.т[аa@o0о]ску|тр[аa@]х.|ф[аa@]к|п.скуд',
+        text.lower())
 
 
 def setup(bot):
