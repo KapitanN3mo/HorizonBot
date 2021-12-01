3b22b3f70cfcadc79e03decf3e3057c946452727
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-12-01 18:54:43 +0300

    отжаривание

--- modules/fun.py
+++ modules/fun.py
@@ -11,6 +11,60 @@ class FunCommands(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    async def fry(self, ctx: commands.Context, user: discord.User):
+        pieces_count = 10
+        old_pieces_count = 10
+        embed = discord.Embed(title=f'Жарим {user.name}', description="**Прогресс отжаривания:**\n" + "<" + "=" + ">",
+                              colour=0xFF8F00)
+        msg = await ctx.send(embed=embed)
+        for i in range(1, 11):
+            embed = discord.Embed(title=f'Жарим {user.name}',
+                                  description="**Прогресс отжаривания:**\n" + "<" + "=" * i + ">" + str(i * 10) + "%",
+                                  colour=0xFF8F00)
+            await msg.edit(embed=embed)
+            await asyncio.sleep(1)
+        embed = discord.Embed(title=f'Жарим {user.name}',
+                              description="**Прогресс отжаривания:**\n" + f"Успешно отжарено! "
+                                                                          f"Хотите кусочек? Осталось {pieces_count} кусочек?",
+                              colour=0xFF8F00)
+        await msg.edit(embed=embed)
+        await msg.add_reaction('🍗')
+        mes_id = msg.id
+        while pieces_count > 0:
+            msg = await ctx.fetch_message(mes_id)
+            emojis = msg.reactions
+            #print(emojis)
+            react_count = None
+            for emoji in emojis:
+                if emoji.emoji == '🍗':
+                    react_count = emoji.count
+                    break
+            if react_count is None:
+                await ctx.send('Кто-то украл всю еду! Вот розьбiйник!')
+                await msg.edit(embed=discord.Embed(title=f'Жарим {user.name}',
+                                                   description="**Прогресс отжаривания:**\n" + f"Успешно отжарено! "
+                                                                                               f"Всё украли! Расходимся!",
+                                                   colour=0xFF8F00))
+                return
+            pieces_count = 11 - react_count
+            if pieces_count == old_pieces_count:
+                continue
+            else:
+                if pieces_count in [1]:
+                    piece_str = 'кусочек'
+                elif pieces_count in [2, 3, 4]:
+                    piece_str = 'кусочка'
+                elif pieces_count in [5, 6, 7, 8, 9, 10]:
+                    piece_str = 'кусочков'
+
+                embed = discord.Embed(title=f'Жарим {user.name}',
+                                      description="**Прогресс отжаривания:**\n" + f"Успешно отжарено! "
+                                                                                  f"Хотите кусочек? Осталось {pieces_count} {piece_str}?",
+                                      colour=0xFF8F00)
+                await msg.edit(embed=embed)
+            await asyncio.sleep(1)
+
+    @commands.command()
     async def kill(self, ctx, user: discord.User, state, channel: int):
         if state in ['true', 'True']:
             state = True
