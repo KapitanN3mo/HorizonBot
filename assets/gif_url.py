374a1e1c91a3f1258899c93d35e936d84b5e2d01
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-12-07 15:06:24 +0300

    Добавлены gif для hug

--- assets/gif_url.py
+++ assets/gif_url.py
@@ -1,5 +1,6 @@
 cookie_gif = ['https://i.gifer.com/c0M.gif']
 hug_gif = ['https://cdn.discordapp.com/attachments/911225950748946443/917720151141515304/hug_014.gif',
            'https://cdn.discordapp.com/attachments/911225950748946443/917720195517284362/hug_074.gif',
-           'https://cdn.discordapp.com/attachments/911225950748946443/917720231126917150/image_860602190646073048924.gif']
+           'https://cdn.discordapp.com/attachments/911225950748946443/917720231126917150/image_860602190646073048924.gif',
+           'https://cdn.discordapp.com/attachments/911254898476216420/917733042221617193/Sk80wyhqz-1.gif']
 # https://i.gifer.com/fybn.gif
