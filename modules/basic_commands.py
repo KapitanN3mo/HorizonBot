a5aabda5215ecaf995e1fc680abeb893eddbdb83
KapitanN3mo <KapitaN3mo@yandex.ru>
2021-12-07 16:09:30 +0300

    Роль Хины добавлена в список admin_roles

--- modules/basic_commands.py
+++ modules/basic_commands.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
-from componets import admin_role
+from componets import admin_role, admin_roles
 
 
 class BasicCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.has_role(admin_role)
+    @commands.has_any_role(admin_roles)
     @commands.command()
-    async def send(self,ctx, channel: discord.TextChannel, *, message: str):
+    async def send(self, ctx, channel: discord.TextChannel, *, message: str):
         print(message)
         await channel.send(content=message)
 
