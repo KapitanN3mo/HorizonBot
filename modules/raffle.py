14099cb2182f2af113dc4d482e859f5bd7efe215
KapitanN3mo <58622959+KapitanN3mo@users.noreply.github.com>
2022-01-19 20:27:55 +0300

    Команда для розыгрышей

--- modules/raffle.py
+++ modules/raffle.py
@@ -1,5 +1,7 @@
+import asyncio
 import datetime
 import json
+import random
 
 import discord
 from discord.ext import commands
@@ -12,7 +14,7 @@ class Raffle(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.has_any_role(*admin_roles)
+    # @commands.has_any_role(*admin_roles)
     @commands.command()
     async def create_raffle(self, ctx: commands.Context, channel: discord.TextChannel, *, data: str):
         try:
@@ -22,12 +24,12 @@ class Raffle(commands.Cog):
             return
         try:
             date = datetime.datetime.strptime(data['date'], datetime_format)
-            show_author = True if data['author'] in ['True', 'true'] else False
+            show_author = data['show_author']  # True if data['show_author'] in ['True', 'true'] else False
             text = data['text']
             winner_count = data['win_count']
             emoji = data['emoji']
-        except KeyError:
-            await ctx.send(":exclamation: `Недостаточно аргументов`")
+        except KeyError as ex:
+            await ctx.send(f":exclamation: `Недостаточно аргументов` {ex}")
             return
         except Exception as ex:
             await ctx.send(f":exclamation: `Неверные данные` Info:{ex}")
@@ -40,9 +42,35 @@ class Raffle(commands.Cog):
         emb.add_field(name='Информация', value=text)
         if show_author:
             emb.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        emb.set_footer(text=f'Окончание в: {date}')
+        emb.set_footer(text=f'Самые честные розыгрыши от HorizonBot!',
+                       icon_url=self.bot.user.avatar_url)
+        emb.add_field(name='Окончание в: ', value=f'{date.strftime("%m-%d-%H-%M")}')
         message = await channel.send(embed=emb)
         await message.add_reaction(emoji=emoji)
+        sleep_time = (date - datetime.datetime.now()).seconds
+        await ctx.send(f'Время ожидания: {sleep_time}')
+        await asyncio.sleep(sleep_time)
+        message = await message.channel.fetch_message(id=message.id)
+        reactions = message.reactions
+        react = [r for r in reactions if r.emoji == emoji][0]
+        winners = []
+        participants = [participant for participant in await react.users().flatten() if
+                        participant.id != self.bot.user.id]
+        for i in range(winner_count):
+            winner = random.choice(participants)
+            winners.append(winner)
+            participants.remove(winner)
+        guild_names = []
+        for winner in winners:
+            name = discord.utils.get(ctx.guild.members, id=winner.id)
+            guild_names.append(name)
+        emb = discord.Embed(title="Поздравляем победителя!",
+                            colour=discord.Colour.magenta())
+        emb.add_field(name='Победители:' if winner_count > 1 else "Победитель:",
+                      value="\n".join(name.display_name for name in guild_names if isinstance(name, discord.Member)))
+        emb.set_footer(text=f'Самые честные розыгрыши от HorizonBot!',
+                       icon_url=self.bot.user.avatar_url)
+        await ctx.send(embed=emb)
 
 
 def setup(bot: commands.Bot):
